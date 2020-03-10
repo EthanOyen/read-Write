db92d7902753d641cdc6134e614e6b7f8fe00c27
EthanOyen <55327075+EthanOyen@users.noreply.github.com>
2020-03-09 23:54:56 -0500

    Initial commit

---
+++ read&Write.py
@@ -0,0 +1,12 @@
+'''
+Created on Feb 29, 2020
+
+@author: ITAUser
+'''
+file1 = "fileName.txt", "mode"
+
+#read - read(), readLine()
+#write - write()
+
+file1 = open("hello.txt", "a")
+file1.close()
